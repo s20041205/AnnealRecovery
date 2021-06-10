4b9cb1c9b7dfced396f423eaabc7c739b21f1831
Alice <stlinalice@gmail.com>
2021-06-10 16:06:41 +0800

    image of overview added

--- AnnealFileRecovery/frmFileRecovery.Designer.cs
+++ AnnealFileRecovery/frmFileRecovery.Designer.cs
@@ -28,21 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnFolderPath = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lblToRecover = new System.Windows.Forms.Label();
+            this.lblPatnID = new System.Windows.Forms.Label();
+            this.cmbAdmit = new System.Windows.Forms.ComboBox();
+            this.lblAdmit = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnFolderPath
+            // 
+            this.btnFolderPath.Location = new System.Drawing.Point(286, 24);
+            this.btnFolderPath.Name = "btnFolderPath";
+            this.btnFolderPath.Size = new System.Drawing.Size(31, 23);
+            this.btnFolderPath.TabIndex = 0;
+            this.btnFolderPath.Text = "...";
+            this.btnFolderPath.UseVisualStyleBackColor = true;
+            this.btnFolderPath.Click += new System.EventHandler(this.btnFolderPath_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 24);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(268, 22);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(263, 88);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(54, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Recover";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lblToRecover
+            // 
+            this.lblToRecover.AutoSize = true;
+            this.lblToRecover.Location = new System.Drawing.Point(12, 64);
+            this.lblToRecover.Name = "lblToRecover";
+            this.lblToRecover.Size = new System.Drawing.Size(63, 12);
+            this.lblToRecover.TabIndex = 3;
+            this.lblToRecover.Text = "To Recover:";
+            // 
+            // lblPatnID
+            // 
+            this.lblPatnID.AutoSize = true;
+            this.lblPatnID.Location = new System.Drawing.Point(81, 64);
+            this.lblPatnID.Name = "lblPatnID";
+            this.lblPatnID.Size = new System.Drawing.Size(59, 12);
+            this.lblPatnID.TabIndex = 4;
+            this.lblPatnID.Text = "(Patient ID)";
+            // 
+            // cmbAdmit
+            // 
+            this.cmbAdmit.FormattingEnabled = true;
+            this.cmbAdmit.Location = new System.Drawing.Point(83, 88);
+            this.cmbAdmit.Name = "cmbAdmit";
+            this.cmbAdmit.Size = new System.Drawing.Size(124, 20);
+            this.cmbAdmit.TabIndex = 5;
+            // 
+            // lblAdmit
+            // 
+            this.lblAdmit.AutoSize = true;
+            this.lblAdmit.Location = new System.Drawing.Point(12, 88);
+            this.lblAdmit.Name = "lblAdmit";
+            this.lblAdmit.Size = new System.Drawing.Size(64, 12);
+            this.lblAdmit.TabIndex = 6;
+            this.lblAdmit.Text = "Time Admit:";
             // 
             // frmFileRecovery
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(329, 233);
+            this.Controls.Add(this.lblAdmit);
+            this.Controls.Add(this.cmbAdmit);
+            this.Controls.Add(this.lblPatnID);
+            this.Controls.Add(this.lblToRecover);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnFolderPath);
             this.Name = "frmFileRecovery";
             this.Text = "AnnealFileRecovery";
             this.Load += new System.EventHandler(this.frmFileRecovery_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnFolderPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lblToRecover;
+        private System.Windows.Forms.Label lblPatnID;
+        private System.Windows.Forms.ComboBox cmbAdmit;
+        private System.Windows.Forms.Label lblAdmit;
     }
 }
 
--- AnnealFileRecovery/frmFileRecovery.cs
+++ AnnealFileRecovery/frmFileRecovery.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace AnnealFileRecovery
@@ -16,14 +15,166 @@ namespace AnnealFileRecovery
         {
             InitializeComponent();
         }
-
+        private string _root = @"C:\VitalData\[VT-Trace]\";
         private void frmFileRecovery_Load(object sender, EventArgs e)
+        {
+            List<string> dirs = ListAllFolder(_root);
+            if (dirs != null && dirs.Count > 0)
+            {
+                txtPath.Text = dirs[0];
+                UpdateInfo(txtPath.Text);
+                GetNumberOfAnneals(txtPath.Text);
+            }
+            else
+            {
+                //
+            }
+
+        }
+        private List<string> ListAllFolder(string root)
+        {
+            List<string> files = new List<string>();
+            foreach (var subDir in Directory.EnumerateDirectories(root))
+            {
+                try
+                {
+                    files.Add(subDir);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    // ...
+                }
+            }
+            return files;
+        }
+        private void btnFolderPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog loadFolderDlg = new FolderBrowserDialog();
+            loadFolderDlg.SelectedPath = _root;
+            if (loadFolderDlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string path = loadFolderDlg.SelectedPath;
+                    UpdateInfo(path);
+                    GetNumberOfAnneals(path);
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+        private void btnRun_Click(object sender, EventArgs e)
         {
 
 
 
 
+        }
+        private void UpdateInfo(string path)
+        {
+            txtPath.Text = path;
+            string dir = Path.GetDirectoryName(path) + "\\";
+            string patnID = Path.GetFileName(path);
+            lblPatnID.Text = patnID;
+            System.Diagnostics.Debug.Print("txtPath.Text={0} => dir={1}, patnID={2}", txtPath.Text, dir, patnID);
+        }
+        //
+        private void GetNumberOfAnneals(string path = null)
+        {
+            if (string.IsNullOrEmpty(path)) return;
+            //[List admits]
+            List<string> lst = new List<string>();
+            if (Directory.Exists(path))
+                lst = ListAllFolder(path);
+            //
+            string rx =
+                @"^(19[5-9][0-9]|20[0-4][0-9]|2050)(0?[1-9]|1[0-2])(0?[1-9]|[12][0-9]|3[01])(([0-1]?[0-9])|(2[0-3]))([0-5][0-9])([0-5][0-9])$";
+            Regex rgx = new Regex(rx);
+            List<string> lstAdmits = new List<string>();
+            for (int i = 0; i < lst.Count; i++)
+            {
+                string admitTag = new DirectoryInfo(lst[i]).Name;
+                if (rgx.IsMatch(admitTag))
+                {
+                    System.Diagnostics.Debug.Print($"[{i}]: {admitTag}");
+                    lstAdmits.Add(lst[i]);
+                }
+            }
+            if (lstAdmits == null) return;
+
+            //[Select one admit]
+            AddAdmitsToComboBox(lstAdmits);
+            //TODO: to select: cmbAdmit select item changed
+            //if lstAdmits.Count > 1 => choose one to explore
+            //- explore and count how many anneals
+            string admit = new DirectoryInfo(lstAdmits[0]).Name;
+
+            //[Check event files and find anneal count]
+            string patnID = Path.GetFileName(path);
+            string fn = lstAdmits[0];
+            string pathEvt = $"{fn}+\\{patnID}_{admit}.event";
+
+
+
 
         }
+
+        private void AddAdmitsToComboBox(List<string> lst)
+        {
+            if (cmbAdmit == null) return;
+            if (lst == null || lst.Count == 0)
+            {
+                cmbAdmit.SelectedIndex = -1;
+                cmbAdmit.Items.Clear();
+                return;
+            }
+            //
+            foreach (var v in lst)
+            {
+                string admitTag = new DirectoryInfo(v).Name;
+                string ts = ToString(admitTag, DT_PATTERN_DISPLAY);
+                cmbAdmit.Items.Add(new KeyValuePair<string, string>(ts, v));
+            }
+            cmbAdmit.DisplayMember = "key";
+            cmbAdmit.ValueMember = "value";
+            cmbAdmit.SelectedIndex = 0;
+        }
+        //[DateTime region]
+        private const string DT_PATTERN_NOBLANK = "yyyyMMddHHmmss";
+        private const string DT_PATTERN_DISPLAY = "yyyy/MM/dd HH:mm:ss";
+        private const string RGX_DT_DISPLAY = @"^[1-2]{1}\d{3}\/\d{1,2}\/[0-3]{1}\d{1}\s+\d{2}:\d{2}:\d{2}$";
+        private const string RGX_DT_NOBLANK = @"^[1-2]{1}\d{13}$";
+        public static string ToString(string src, string pattern = DT_PATTERN_DISPLAY)
+        {
+            if (Regex.IsMatch(src, RGX_DT_DISPLAY, RegexOptions.None))
+            {
+                string dest;
+                try
+                {
+                    dest = DateTime.Parse(src).ToString(pattern);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print("TimeUtil Exception={0}", ex);
+                    throw;
+                }
+                return dest;
+            }
+            if (Regex.IsMatch(src, RGX_DT_NOBLANK, RegexOptions.None))
+            {
+                return ToDateTime(src, DT_PATTERN_NOBLANK).ToString(pattern);
+            }
+            return "";
+        }
+        private static DateTime ToDateTime(string str, string pattern = DT_PATTERN_NOBLANK)
+        {
+            DateTime dt;
+            DateTime.TryParseExact(str, pattern, null, System.Globalization.DateTimeStyles.None, out dt);
+            return dt;
+        }
+        //
     }
 }
